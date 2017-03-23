168e0d0292d547c972d426baa2bf08c725039383
Ariel Hernández <ariel.h.estevenz@ieee.org>
2017-03-23 18:36:44 -0300

    ADD: Python script to clean image noise (getCleanImage.py)

---
+++ getCleanImage.py
@@ -0,0 +1,21 @@
+import sys
+from GammaDetection import GammaDetection
+
+# Variables
+path_source=sys.argv[1] # Path to the essay directory with the radioactive sources (images)
+path_blackout=sys.argv[2] # Path to the essay directory without the radioactive sources (images), the acquisition was in blackout
+image_index=int(sys.argv[3]) # Image index for the image processing in the essay directory
+
+# Script
+source = GammaDetection(path_source, image_index) # Object with radioactive source
+blackout = GammaDetection(path_blackout, image_index) # Object without radioactive source
+
+img_src = source.getSelectedImage()# Image from essay with sources
+img_blk = blackout.getSelectedImage()# Image from essay without sources
+
+source.getHistSelectedImage(1)# Histogram of image with sources
+blackout.getHistSelectedImage(2)# Histogram of image in blackout (a lot of noise)
+
+# A way to reduce the camera noise
+source.setSubtractImage(img_blk)# Differences between image with the radioactive source and blackout
+source.getHistSubtractImage(3)# Histogram of the difference image
