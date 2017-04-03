56a2671b1c7b96300848ccda7a507766fc312ee4
Ariel Hernández <ariel.h.estevenz@ieee.org>
2017-04-03 08:01:07 -0300

    MOD: Just for cleaning image (getCleanImage.py)
    I modified this script only for image cleaning purposes, I will make a new one for calibration (edge detector and filtering).

--- getCleanImage.py
+++ getCleanImage.py
@@ -1,19 +1,17 @@
-import sys
 from GammaDetection import GammaDetection
 import cv2
-import numpy as np
 import argparse
 
 # Args parser
 ap = argparse.ArgumentParser()
 ap.add_argument("-c", "--source", required=True,
-	help="Path to essay directory of images acquire with radioactive sources")
+	help="path to essay directory of images acquire with radioactive sources")
 ap.add_argument("-b", "--background", required=True,
-	help="Path to essay directory of images acquire in background")
+	help="path to essay directory of images acquire in background")
 ap.add_argument("-i", "--index", required=True, type=int,
-	help="Image index for the image processing in the essay directory")
+	help="image index for the image processing in the essay directory")
 ap.add_argument("-s", "--show", default=False, type=bool,
-	help="Show graphics")
+	help="show graphics")
 args = vars(ap.parse_args())
 
 # Variables
@@ -39,14 +37,11 @@ if show==True:
     blackout.getHistSelectedImage(2, "Histogram of image in blackout")# Histogram of image in blackout (a lot of noise)
     source.getHistSubtractImage(3, "Histogram of the difference image")# Histogram of the difference image
 
-# Canny Detector
+# Without R channel
 img_diff_r_0 = source.setZeroChannel(0,img_diff) # deletes R channel
 img_diff_r_0_ocv = source.convertPILtoCV(img_diff_r_0) # to OpenCV
-edges_median=source.autoCannyDetector(img_diff_r_0_ocv) # edges
 
 if show==True:
     cv2.namedWindow('Image difference (without red channel)', cv2.WINDOW_NORMAL)
-    cv2.namedWindow('Image edges', cv2.WINDOW_NORMAL)
-    cv2.imshow('Image difference (without red channel)',img_diff_r_0_ocv)
-    cv2.imshow('Image edges',edges_median)
+    cv2.imshow('Image difference (without red channel)', img_diff_r_0_ocv)
     cv2.waitKey(0)
