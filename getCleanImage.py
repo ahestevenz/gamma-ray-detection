fb09d0aa25e808838c792ca537a649b549d8b39b
Ariel Hernández <ariel.h.estevenz@ieee.org>
2017-04-17 11:29:42 -0300

    ADD: Args parser (getCleanImage.py)

--- getCleanImage.py
+++ getCleanImage.py
@@ -5,23 +5,29 @@ import argparse
 ### Args parser
 ap = argparse.ArgumentParser()
 ap.add_argument("-r", "--radioactive_source", required=True,
-	help="path to essay directory of images acquire with radioactive sources")
+	            help="path to essay directory of images acquire with radioactive sources")
 ap.add_argument("-b", "--background", required=True,
-	help="path to essay directory of images acquire in background")
+	            help="path to essay directory of images acquire in background")
 ap.add_argument("-i", "--index", required=True, type=int,
-	help="image index for the image processing in the essay directory")
+	            help="image index for the image processing in the essay directory")
 ap.add_argument("-f", "--file_calib", default="./conf/config.calib", type=str,
-	help="calibration file location")
-ap.add_argument("-s", "--show", default=False, type=bool,
-	help="show graphics")
+	            help="calibration file location")
+ap.add_argument("-s", "--show", action='store_true',
+    			help="show graphics")
+ap.add_argument("-v", "--verbose", action='store_true',
+    			help="verbose mode")
+ap.add_argument("-w", "--write_images", action='store_true',
+				help="save images")
 args = vars(ap.parse_args())
 
 ### Variables
 path_source=args["radioactive_source"] # Path to the essay directory with the radioactive sources (images)
 path_blackout=args["background"] # Path to the essay directory without the radioactive sources (images), the acquisition was in blackout
 image_index=args["index"] # Image index for the image processing in the essay directory
-show = args["show"] # Show images
 file_calib_location = args["file_calib"] # Calibration file location
+show = args["show"] # Show images and graphics
+verbose = args["verbose"] # Verbose mode
+write = args["write_images"]  # Save images
 
 ### Script
 source = GammaDetection(path_source, image_index) # Object with radioactive source
@@ -43,6 +49,7 @@ if show==True:
 # Without R channel
 img_diff_clean = source.setZeroChannel(0,img_diff) # deletes R channel
 img_diff_clean_ocv = source.convertPILtoCV2(img_diff_clean) # to OpenCV
+img_diff_clean_ocv_grid = source.convertPILtoCV2(img_diff_clean) # to OpenCV
 
 if show==True:
     cv2.namedWindow('Image difference (without red channel)', cv2.WINDOW_NORMAL)
