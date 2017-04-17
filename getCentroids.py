4da6183cd3500631cc688e13c6fbb6def7fa27b2
Ariel Hernández <ariel.h.estevenz@ieee.org>
2017-04-17 11:28:31 -0300

    ADD: Get calib parameters (getCentroids.py)
    Get the calibration parameterers from file and refine script arguments.

--- getCentroids.py
+++ getCentroids.py
@@ -5,9 +5,9 @@ import os
 import sys
 import warnings
 import getCleanImage
-import matplotlib.pyplot as plt
 
 # For arguments, please check getCleanImage script
+### Variables
 if os.path.exists(getCleanImage.file_calib_location):
     try:
         file_calib = open(getCleanImage.file_calib_location, 'r')
@@ -18,15 +18,33 @@ else:
     print "File does not exist:", file_calib
     sys.exit()
 
-x, y, width, height = (404, 314, 500, 511)#(400, 304, 516, 536)
-angle = 0.0174533446603
-mat_centroids = getCleanImage.source.getSquaresValues(x, y, width, height)
+# Getting calib parameters
+coor = file_calib.readline().rstrip('\n').translate(None,'()').split(",")
+angle = float(file_calib.readline().rstrip('\n'))
+x = int(coor[0])
+y = int(coor[1])
+width = int(coor[2])
+height = int(coor[3])
+file_calib.close()
 
-print mat_centroids
+### Script
+if getCleanImage.verbose==True:
+    print "\nCalibration values \n| x | y | width | height |"
+    print '|',x,'|',y,'|',width,'|',height,'|'
 
+if getCleanImage.show==True:
+    getCleanImage.source.getSubtractGrayImage().show()
+    getCleanImage.source.makeGrid(x, y, width, height, getCleanImage.img_diff_clean_ocv_grid)
+    cv2.namedWindow('Image: Grid', cv2.WINDOW_NORMAL)
+    cv2.imshow('Image: Grid', getCleanImage.img_diff_clean_ocv_grid)
+    cv2.waitKey(0)
 
-c = plt.pcolor(mat_centroids, edgecolors='k', linewidths=1)
-plt.gca().invert_yaxis()
-plt.title('thick alreve')
+if getCleanImage.write==True:
+    cv2.imwrite("img_diff_ocv_grid.jpg", getCleanImage.img_diff_clean_ocv_grid);
 
-plt.show()
+mat_centroids = getCleanImage.source.getPlotSuperPixelMatrix(x, y, width, height, getCleanImage.verbose)
+
+if getCleanImage.verbose==True:
+    np.set_printoptions(precision=2)
+    print "\nCentroids Matrix"
+    print mat_centroids
